2cf790b7e9874df8a539c1fe3a81885c9690c34b
Saverio Follador <saverio@moku.io>
2019-03-08 21:19:27 +0100

    minor fix

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
+const port = process.env.PORT || 3000;
+const websocketPort = process.env.WEBSOCKET_PORT || 40510;
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const WebSocketServer = require('ws').Server;
-const wss = new WebSocketServer({port: 40510});
 const app = express();
-
-const port = process.env.PORT || 3000;
-
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
+
+const WebSocketServer = require('ws').Server;
+const wss = new WebSocketServer({port: websocketPort});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -27,11 +28,6 @@ app.post('/play', (req, res) => {
     data: req.body.text
   })));
   res.send(`Playing ${req.body.text} :sound:`);
-});
-
-app.get('/send', (req, res) => {
-  wss.clients.forEach(client => client.send({}));
-  res.send(200);
 });
 
 app.listen(port, () => {
